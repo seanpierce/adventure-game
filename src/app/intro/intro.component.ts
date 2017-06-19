46485d7b896699d0eda8f78f391bc70bcb0d5d8d
Ben Williams and Sean Pierce <ben@>
2017-06-19 11:21:48 -0700

    add animations to background

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -9,12 +9,29 @@ import { Character } from '../character.model';
 export class IntroComponent implements OnInit {
 
   player: Character;
+  question1 = true;
 
   constructor() {
     this.player = new Character(10, 10, 10);
   }
 
   ngOnInit() {
+  }
+
+  addStr(player) {
+    player.strength += 2;
+    player.intelligence -= 1;
+    player.charisma -= 1;
+  }
+  addInt(player) {
+    player.intelligence += 2;
+    player.strength -= 1;
+    player.charisma -= 1;
+  }
+  addChar(player) {
+    player.charisma += 2;
+    player.intelligence -= 1;
+    player.strength -= 1;
   }
 
 }
